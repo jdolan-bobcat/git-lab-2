f8159e8670df830dca9b2c1ed1aee1453c797d99
John Dolan <dolan@ohio.edu>
2019-08-17 09:03:50 -0400

    Initial Comment

---
+++ git-lab-program.cc
@@ -0,0 +1,21 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Enter your name>
+ *        Date: <Enter today's date>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout<<"Hello Git!\n";
+    cout<<"This is the Minoho.";
+    return 0;
+}// main
+
